7116bb55d65abdb1a82fc12c1b616a690209db7a
Rui Zhong <zhongrui19961230@gmail.com>
2023-04-08 12:43:27 +0900

    first commit

--- test.py
+++ test.py
@@ -1,28 +1,92 @@
-from scipy.stats.stats import mannwhitneyu
 import numpy as np
-import csv
+from copy import deepcopy
 
 
-def open_csv(path):
-    data = []
-    with open(path) as f:
-        f_csv = csv.reader(f)
-        for row in f_csv:
-            d = []
-            for s in row:
-                d.append(float(s))
-            data.append(d)
-    return np.array(data)
+def diag_multi(matrix, vector):
+     r, c = matrix.shape
+     for i in range(r):
+          for j in range(c):
+               matrix[i][j] *= vector[j]
+     return matrix
 
 
-path_VEGE = "C:\\Users\\WindAsMe\\Downloads\\Vegetation Evolution_Python\\VEGE_Data\\F4_10D.csv"
-path_iVEGE = "C:\\Users\\WindAsMe\\Downloads\\Vegetation Evolution_Python\\iVEGE_Data\\F4_10D.csv"
-VEGE_obj = open_csv(path_VEGE)
-iVEGE_obj = open_csv(path_iVEGE)
+def self_multi(vector):
+     t_vector = deepcopy(vector)
+     for i in range(len(t_vector)):
+          t_vector[i] *= t_vector[i]
+     return np.array(t_vector)
 
-print(VEGE_obj)
-final_VEGE = VEGE_obj[:, len(VEGE_obj[0])-1]
-final_iVEGE = iVEGE_obj[:, len(iVEGE_obj[0])-1]
-print(final_VEGE, np.mean(final_VEGE))
-print(final_iVEGE, np.mean(final_iVEGE))
-print(mannwhitneyu(final_VEGE, final_iVEGE))
+
+def binary_random(m, M):
+     """
+     :param m: scale of phi
+     :param M: budget
+     :return:
+     """
+     tM = np.random.choice(list(range(0, M+1)))
+
+     phi = np.zeros(m)
+     index = np.random.choice(list(range(0, m)), tM)
+     for i in index:
+          phi[i] = 1
+     return phi
+
+
+def isNegative(vector):
+     for e in vector:
+          if e > 0:
+               return False
+     return True
+
+
+def cons(phi, b, H):
+     """
+     :param phi: binary decision variables
+     :param b: continuous decision variables
+     :param H: relationship matrix
+     :return:
+     """
+     n, m = H.shape
+     R = np.ones(n)
+     f = list(range(m, 0, -1))
+
+     H_hat = diag_multi(diag_multi(H, phi), f)
+     b_hat = self_multi(b).reshape(-1, 1)
+
+     temp = np.dot(H_hat, b_hat)[:, 0]
+     constraints = R - temp
+     return constraints
+
+
+if __name__ == "__main__":
+     H = np.array([[1, 1, 0, 0, 1, 1],
+                   [0, 0, 1, 0, 1, 0],
+                   [0, 1, 0, 1, 0, 1],
+                   [0, 0, 1, 1, 1, 1]])
+     n, m = H.shape
+     M = 2
+
+     for i in range(10000):
+          phi = binary_random(m, M)
+          b = np.random.random(m)
+          # phi = [0, 1, 1, 0, 0, 0]
+          # b = [0.00000032, 0.447215507501615, 0.500001607984798, 0.00000032, 0.00000032, 0.00000032]
+          constraints = cons(phi, b, H)
+          if isNegative(constraints):
+               print("optimum: ", sum(b))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
